7c73f78605304ec5ac38116962b388c517590523
Vinicius Leonardo Silveira <vinicius.silveira1503@gmail.com>
2025-05-23 22:06:58 -0300

    CADASTRO - css+html
    arquivos mudados:
    
    form-cliente
    form-cuidador
    css

--- resources/views/App/form-cuidador.blade.php
+++ resources/views/App/form-cuidador.blade.php
@@ -6,66 +6,74 @@
 @section('content')
     <div class="form-cliente-container">
         <section class="form-cliente">
-            <h1>Cadastre-se Gratuitamente ?</h1>
+            <h1>Cadastre-se Gratuitamente!</h1>
             <form action="{{ route('form.cuidador.submit') }}" method="post" enctype="multipart/form-data">
                 @csrf
 
                 <input type="hidden"name="papel" value="cuidador">
+                
+                <div class="form-cliente-pessoal">
+                    <div>
+                        <label for="nome">Nome</label>
+                        <input type="text"name="nome" class="form-control">
+                    </div>
 
-                <div>
-                    <label for="nome">Nome</label>
-                    <input type="text"name="nome" class="form-control">
+                    <div>
+                        <label for="cpf">Cpf</label>
+                        <input type="text"name="cpf" class="form-control">
+                    </div>
+                    
+                    <div>
+                        <label for="email">E-mail</label>
+                        <input type="email"name="email" class="form-control">
+                    </div>
+
+                    <div>
+                        <label for="telefone">Telefone</label>
+                        <input type="text"name="telefone" class="form-control">
+                    </div>
+
                 </div>
 
-                <div>
-                    <label for="email">E-mail</label>
-                    <input type="email"name="email" class="form-control">
-                </div>
+                <div class="form-cliente-endereco">
 
-                <div>
-                    <label for="telefone">Telefone</label>
-                    <input type="text"name="telefone" class="form-control">
+                    <div>
+                        <label for="cidade">Cidade</label>
+                        <input type="text"name="cidade" class="form-control">
+                    </div>
+                    
+                    <div>
+                        <label for="bairro">Bairro</label>
+                        <input type="text"name="bairro" class="form-control">
+                    </div>
+                    
+                    <div>
+                        <label for="rua">Rua</label>
+                        <input type="text"name="rua" class="form-control">
+                    </div>
                 </div>
-
-                <div>
-                    <label for="cpf">Cpf</label>
-                    <input type="text"name="cpf" class="form-control">
-                </div>
-
-                <div>
-                    <label for="cidade">Cidade</label>
-                    <input type="text"name="cidade" class="form-control">
-                </div>
-
-                <div>
-                    <label for="bairro">Bairro</label>
-                    <input type="text"name="bairro" class="form-control">
-                </div>
-
-                <div>
-                    <label for="rua">Rua</label>
-                    <input type="text"name="rua" class="form-control">
-                </div>
-
-                <div>
-                    <label for="password">Senha</label>
-                    <input type="password"name="password" class="form-control">
-                    @error('password')
-                        <div class="text-warning">
-                            {{ $message }}
+                    
+                    <div class="form-cliente-senha">
+                        <div>
+                            <label for="password">Senha</label>
+                            <input type="password"name="password" class="form-control">
+                            @error('password')
+                            <div class="text-warning">
+                                {{ $message }}
+                            </div>
+                            @enderror
                         </div>
-                    @enderror
-                </div>
-
-                <div>
-                    <label for="password_confirmation">Confirmar Senha</label>
-                    <input type="password"name="password_confirmation" class="form-control">
-                    @error('password_confirmation')
-                        <div class="text-warning">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
+                        
+                        <div>
+                            <label for="password_confirmation">Confirmar Senha</label>
+                            <input type="password"name="password_confirmation" class="form-control">
+                            @error('password_confirmation')
+                            <div class="text-warning">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                                        </div>
+                    </div>
 
                 <div>
                     <label for="foto">Foto</label>
@@ -77,9 +85,9 @@
                     <input type="file"name="curriculo" class="form-control">
                 </div>
 
-                <div>
-                    <input type="submit"class="btn btn-secondary form-control mt-4" value="Cadastrar-se">
-                </div>
+                    <div>
+                        <input type="submit"class="btn btn-secondary form-control mt-4" value="Cadastrar-se">
+                    </div>
             </form>
         </section>
 
